7707c8421a3bc7e12a98e8abc7576dbc706a9a47
Phil Hauser <phil@zweck.co>
2020-02-17 21:41:24 +0000

    Add ifa logo and branding

--- src/services/globalstorage/globalstorage.ts
+++ src/services/globalstorage/globalstorage.ts
@@ -9,8 +9,7 @@ export class GlobalStorage {
     loginVars: false,
     type: ""
   };
-  public primaryDomain: string = 'http://139.59.178.1:4000/';
-  // public primaryDomain: string = 'http://refersquare-dev.businessapps.co.uk';
+  public primaryDomain: string = 'http://157.245.36.90:4000/';
   public baseUrl: string = `${this.primaryDomain}/api`;
   public user: any;
 
