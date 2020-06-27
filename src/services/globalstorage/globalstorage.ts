0d8aff45f4cc62c05a69fbab650e76d8b650777c
Phil Hauser <phil@zweck.co>
2020-06-27 15:11:05 +0100

    Point to new server

--- src/services/globalstorage/globalstorage.ts
+++ src/services/globalstorage/globalstorage.ts
@@ -9,7 +9,7 @@ export class GlobalStorage {
     loginVars: false,
     type: ""
   };
-  public primaryDomain: string = 'http://157.245.36.90:4000/';
+  public primaryDomain: string = 'http://ifa.zweck.software:4000/';
   public baseUrl: string = `${this.primaryDomain}/api`;
   public user: any;
 
